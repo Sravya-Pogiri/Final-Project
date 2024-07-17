3bf51543cb4807a3e800ea61ed60cd2aee484c71
Paige Randell <fulalove2@gmail.com>
2024-07-17 17:03:16 -0400

    the front page of the code

---
+++ Final Project/aboutUndies.swift
@@ -0,0 +1,18 @@
+//
+//  aboutUndies.swift
+//  Final Project
+//
+//  Created by paige randell on 7/17/24.
+//
+
+import SwiftUI
+
+struct aboutUndies: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    aboutUndies()
+}
---
+++ Final Project/productsTab.swift
@@ -0,0 +1,18 @@
+//
+//  productsTab.swift
+//  Final Project
+//
+//  Created by paige randell on 7/17/24.
+//
+
+import SwiftUI
+
+struct productsTab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    productsTab()
+}
